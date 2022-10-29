ce4f912b02e03e982603395104e82c76bb2ddbac
이석호 <mercen@mercen.net>
2022-10-29 15:09:44 +0900

    Initial Commit

---
+++ Exchange/ExchangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExchangeApp.swift
+//  Exchange
+//
+//  Created by Mercen on 2022/10/29.
+//
+
+import SwiftUI
+
+@main
+struct ExchangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
